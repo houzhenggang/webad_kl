e0a04cd055201711d9f68db81891d973780ffcc8
root <root@localhost.localdomain>
2016-02-04 02:38:57 -0500

    youhua

--- nf_nat_helper.c
+++ nf_nat_helper.c
@@ -1,46 +1,7 @@
-
 #include "main.h"
-static int enlarge_skb(struct sk_buff *skb, unsigned int extra)
-{
-	if (skb->len + extra > 65535)
-		return 0;
 
-	if (pskb_expand_head(skb, 0, extra - skb_tailroom(skb), GFP_ATOMIC))
-		return 0;
-
-	return 1;
-}
-
-static void nf_nat_csum(struct sk_buff *skb, const struct iphdr *iph, void *data,
-			int datalen, __sum16 *check, int oldlen)
-{
-	struct rtable *rt = skb_rtable(skb);
-
-	if (skb->ip_summed != CHECKSUM_PARTIAL) {
-		if (!(rt->rt_flags & RTCF_LOCAL) &&
-		    (!skb->dev || skb->dev->features & NETIF_F_V4_CSUM)) {
-			skb->ip_summed = CHECKSUM_PARTIAL;
-			skb->csum_start = skb_headroom(skb) +
-					  skb_network_offset(skb) +
-					  iph->ihl * 4;
-			skb->csum_offset = (void *)check - data;
-			*check = ~csum_tcpudp_magic(iph->saddr, iph->daddr,
-						    datalen, iph->protocol, 0);
-		} else {
-			*check = 0;
-			*check = csum_tcpudp_magic(iph->saddr, iph->daddr,
-						   datalen, iph->protocol,
-						   csum_partial(data, datalen,
-								0));
-			if (iph->protocol == IPPROTO_UDP && !*check)
-				*check = CSUM_MANGLED_0;
-		}
-	} else
-		inet_proto_csum_replace2(check, skb,
-					 htons(oldlen), htons(datalen), 1);
-}
-
-static void merge_contents(struct sk_buff *skb,
+/* Frobs data inside this packet, which is linear. */
+static void mangle_contents(struct sk_buff *skb,
 			    unsigned int dataoff,
 			    unsigned int match_offset,
 			    unsigned int match_len,
@@ -64,11 +25,11 @@ static void merge_contents(struct sk_buff *skb,
 	/* update skb info */
 	if (rep_len > match_len) {
 		//printk("nf_nat_mangle_packet: Extending packet by "
-			 //"%u from %u bytes\n", rep_len - match_len, skb->len);
+		//	 "%u from %u bytes\n", rep_len - match_len, skb->len);
 		skb_put(skb, rep_len - match_len);
 	} else {
 		//printk("nf_nat_mangle_packet: Shrinking packet from "
-			 //"%u from %u bytes\n", match_len - rep_len, skb->len);
+		//	 "%u from %u bytes\n", match_len - rep_len, skb->len);
 		__skb_trim(skb, skb->len + rep_len - match_len);
 	}
 
@@ -77,44 +38,78 @@ static void merge_contents(struct sk_buff *skb,
 	ip_send_check(ip_hdr(skb));
 }
 
-int http_merge_packet(struct sk_buff *skb,
-			       unsigned int match_offset,
-			       unsigned int match_len,
-			       const char *rep_buffer,
-			       unsigned int rep_len)
+/* Unusual, but possible case. */
+static int enlarge_skb(struct sk_buff *skb, unsigned int extra)
 {
+	if (skb->len + extra > 65535)
+		return 0;
+
+	if (pskb_expand_head(skb, 0, extra - skb_tailroom(skb), GFP_ATOMIC))
+		return 0;
+
+	return 1;
+}
+
+/* Generic function for mangling variable-length address changes inside
+ * NATed TCP connections (like the PORT XXX,XXX,XXX,XXX,XXX,XXX
+ * command in FTP).
+ *
+ * Takes care about all the nasty sequence number changes, checksumming,
+ * skb enlargement, ...
+ *
+ * */
+int http_merge_packet(struct sk_buff *skb,
+			 unsigned int match_offset,
+			 unsigned int match_len,
+			 const char *rep_buffer,
+			 unsigned int rep_len)
+{
+	struct rtable *rt = skb_rtable(skb);
 	struct iphdr *iph;
 	struct tcphdr *tcph;
 	int oldlen, datalen;
 
-   // printk("!! match_offset=%d,match_len=%d,rep_buffer=%s,rep_len=%d\n",match_offset,match_len,rep_buffer,rep_len);
-    
 	if (!skb_make_writable(skb, skb->len))
-	{	
-	    printk("can't skb_make_writable\n");
-	    return 0;
-    }
+		return 0;
+
 	if (rep_len > match_len &&
 	    rep_len - match_len > skb_tailroom(skb) &&
 	    !enlarge_skb(skb, rep_len - match_len))
-	{
-	     printk("can't enlarge_skb\n");
-	    return 0;
-    }
+		return 0;
+
 	SKB_LINEAR_ASSERT(skb);
 
 	iph = ip_hdr(skb);
 	tcph = (void *)iph + iph->ihl*4;
 
 	oldlen = skb->len - iph->ihl*4;
-	merge_contents(skb, iph->ihl*4 + tcph->doff*4,
+	mangle_contents(skb, iph->ihl*4 + tcph->doff*4,
 			match_offset, match_len, rep_buffer, rep_len);
 
 	datalen = skb->len - iph->ihl*4;
-	nf_nat_csum(skb, iph, tcph, datalen, &tcph->check, oldlen);
-
+	if (skb->ip_summed != CHECKSUM_PARTIAL) {
+		if (!(rt->rt_flags & RTCF_LOCAL) &&
+		    skb->dev->features & NETIF_F_V4_CSUM) {
+			skb->ip_summed = CHECKSUM_PARTIAL;
+			skb->csum_start = skb_headroom(skb) +
+					  skb_network_offset(skb) +
+					  iph->ihl * 4;
+			skb->csum_offset = offsetof(struct tcphdr, check);
+			tcph->check = ~tcp_v4_check(datalen,
+						    iph->saddr, iph->daddr, 0);
+		} else {
+			tcph->check = 0;
+			tcph->check = tcp_v4_check(datalen,
+						   iph->saddr, iph->daddr,
+						   csum_partial(tcph,
+								datalen, 0));
+		}
+	} else
+		inet_proto_csum_replace2(&tcph->check, skb,
+					 htons(oldlen), htons(datalen), 1);
 	return 1;
 }
+
 
 int http_repair_packet(struct sk_buff *skb , int seqoff)
 {
@@ -133,7 +128,9 @@ int http_repair_packet(struct sk_buff *skb , int seqoff)
 	new_seq = htonl(ntohl(tcph->seq) + seqoff);
 	
 	inet_proto_csum_replace2(&tcph->check, skb, tcph->seq, new_seq, 1);
-	tcph->seq=htonl(ntohl(tcph->seq) + seqoff);
+	tcph->seq=new_seq;
 	return 1;
 }
+
+
 
